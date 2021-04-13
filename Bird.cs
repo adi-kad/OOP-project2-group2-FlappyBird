b48d92413f3f5a366cacb651b1e6cb9d2f24e744
Adnan Kadrispahic <adikadrispahic@gmail.com>
2021-04-13 15:11:26 +0200

    Some classes added

---
+++ Bird.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FlappyBird
+{
+    class Bird
+    {
+        public int x;
+        public int y;
+        int fallSpeed;
+
+        public Bird(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+
+        public Bird()
+        {
+            this.x = 25;
+            this.y = 15;
+        }
+
+        public void Jump() 
+        {
+        }
+
+        public void Fall() 
+        {
+        }
+    
+        public void DrawBird()
+        {
+            Console.SetCursorPosition(this.x, this.y);
+            Console.WriteLine("<>?<>");
+        }
+
+    }
+}
---
+++ Board.cs
@@ -0,0 +1,67 @@
+﻿using System;
+
+namespace FlappyBird
+{
+    partial class Program
+    {
+        class Board
+        {
+            int height;
+            int width;
+            Bird bird;
+
+            public Board()
+            {
+                this.height = 30;
+                this.width = 100;
+                this.bird = new Bird();
+            }
+
+            public Board(int height, int width)
+            {
+                this.height = height;
+                this.width = width;
+            }
+
+            public void DrawBoard()
+            {
+
+                for (int x = 0; x < width; x++)
+                {
+                    
+                    if (x == width-1)
+                    {
+                        Console.WriteLine("─");
+                    }
+                    else { 
+                        Console.Write("─"); 
+                    }
+                    
+                }
+
+                for (int y = 0; y < height; y++)
+                {
+                    bird.DrawBird();
+                }
+
+
+                for (int x = 0; x < width; x++)
+                {
+
+                    if (x == width - 1)
+                    {
+                        Console.WriteLine("─");
+                    }
+                    else
+                    {
+                        Console.Write("─");
+                    }
+
+                }
+             
+            }
+
+        }
+
+    }
+}
