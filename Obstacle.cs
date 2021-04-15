abb27939025205b765b7907dc7bbe1e4d7397c9a
ChewCup <72440370+ChewCup@users.noreply.github.com>
2021-04-15 13:06:57 +0200

    IDimension
    Interface

--- Obstacle.cs
+++ Obstacle.cs
@@ -4,8 +4,9 @@ using System.Text;
 
 namespace FlappyBird
 {
-    class Obstacle
+    class Obstacle : IDimensions
     {
-
+        public int width { get; set; }
+        public int height { get; set; }
     }
 }
